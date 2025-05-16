981697506a6ff4729a51be5f6a56e8f059fb2c0a
nicolasuniandes <ne.escobar@uniandes.edu.co>
2025-05-16 12:36:53 -0500

    added findall tests to service

--- src/biblioteca/biblioteca.service.spec.ts
+++ src/biblioteca/biblioteca.service.spec.ts
@@ -4,10 +4,12 @@ import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { BibliotecaEntity } from './biblioteca.entity/biblioteca.entity';
 import { BibliotecaService } from './biblioteca.service';
+import { faker } from '@faker-js/faker';
 
 describe('BibliotecaService', () => {
   let service: BibliotecaService;
   let repository: Repository<BibliotecaEntity>;
+  let bibliotecasList: BibliotecaEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,10 +19,32 @@ describe('BibliotecaService', () => {
 
     service = module.get<BibliotecaService>(BibliotecaService);
     repository = module.get<Repository<BibliotecaEntity>>(getRepositoryToken(BibliotecaEntity));
+
+    await seedDatabase();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    bibliotecasList = [];
+    for (let i = 0; i < 5; i++) {
+      const biblioteca: BibliotecaEntity = await repository.save({
+        nombre: faker.company.name(),
+        direccion: faker.location.streetAddress(),
+        ciudad: faker.location.city(),
+        horario: faker.string.alphanumeric()
+      })
+      bibliotecasList.push(biblioteca);
+    }
+  }
+
+  it('findAll should return all museums', async () => {
+    const biblioteca: BibliotecaEntity[] = await service.findAll();
+    expect(biblioteca).not.toBeNull();
+    expect(biblioteca).toHaveLength(bibliotecasList.length);
   });
 
 });
