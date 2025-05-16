95b3c9f77734e038e53d9a266e605cfd07bff64c
nicolasuniandes <ne.escobar@uniandes.edu.co>
2025-05-16 12:03:17 -0500

    created biblioteca services with findall, findone, create, update and delete methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BibliotecaModule } from './biblioteca/biblioteca.module';
 import { LibroModule } from './libro/libro.module';
+import { BibliotecaEntity } from './biblioteca/biblioteca.entity/biblioteca.entity';
+import { LibroEntity } from './libro/libro.entity/libro.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BibliotecaModule, LibroModule],
+  imports: [BibliotecaModule, LibroModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'biblioteca',
+      entities: [BibliotecaEntity, LibroEntity],
+      dropSchema: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/biblioteca/biblioteca.module.ts
+++ src/biblioteca/biblioteca.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BibliotecaService } from './biblioteca.service';
+import { BibliotecaEntity } from './biblioteca.entity/biblioteca.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  providers: [BibliotecaService],
+  imports: [TypeOrmModule.forFeature([BibliotecaEntity])],
+})
 export class BibliotecaModule {}
