a3f4657996597bae4821c7af82b122bfce321ac6
nicolasuniandes <ne.escobar@uniandes.edu.co>
2025-05-17 11:48:40 -0500

    fixed module imports for Libro

--- src/libro/libro.module.ts
+++ src/libro/libro.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LibroService } from './libro.service';
+import { LibroEntity } from './libro.entity/libro.entity';
 import { LibroController } from './libro.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [LibroService],
+  imports: [TypeOrmModule.forFeature([LibroEntity])],
   controllers: [LibroController]
 })
 export class LibroModule {}
